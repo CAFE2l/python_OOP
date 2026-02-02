ef18d0b72785e26e9366391625773ba9b92f4e46
CAFÉ <pciskolargx@gmail.com>
2026-02-02 13:44:07 -0300

    exercise number 3 done

--- Exercicios/ex003/index.py
+++ Exercicios/ex003/index.py
@@ -4,19 +4,22 @@ class ContaBancaria:
         self.id = id
         self.titular = nome
         self. saldo = saldo
-    
+        print(f"conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}")    
     def __str__(self):
         return f"A conta {self.id} de {self.titular} tem R${self.saldo:,.2f} de saldo."
 
     def deposito(self, valor):
         self.saldo += valor
-
+        print(f"Depósito de R${valor:,.2f} autorizado na conta {self.id}")
     def saque(self, valor):
-        self.saldo -= valor
-
+        if valor > self.saldo:
+            print(f"Saque de R${valor:,.2f} recusado. Saldo insuficiente na conta {self.id}")
+        else:
+            self.saldo -= valor
+            print(f"saque de R${valor:,.2f} autorizado na conta {self.id}")
 
 
 c1 = ContaBancaria(122, "CAFÉ", 500)
 c1.deposito(500)
-c1.saque(600)
+c1.saque(6000)
 print(c1)
